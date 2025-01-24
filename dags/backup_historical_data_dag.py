19b2a44211259c547a8933eaffbabd957e3cb872
Rooam Lee <leewr9@naver.com>
2025-01-24 21:44:22 +0900

    dags:: fix historical data

--- dags/backup_historical_data_dag.py
+++ dags/backup_historical_data_dag.py
@@ -22,8 +22,10 @@ def backup_historical_data_dag():
 
     @task
     def backup_past_data() -> list:
-        current_content_no = int(Variable.get("start_content_no", default_var=385))
-        saved_file_paths = []
+        start_content_no = Variable.get("start_content_no", default_var=385)
+        current_content_no = int(start_content_no)
+        
+        backed_up_posts = []
         current_time = pendulum.now()
 
         logger.info(f"Starting backup from content_no: {current_content_no}")
@@ -37,38 +39,38 @@ def backup_historical_data_dag():
             if post_datetime >= current_time:
                 logger.info(f"Post date {post_datetime} is greater than current date. Stopping backup.")
                 break
-
-            data_dir = Variable.get("data_dir")
-            parquet_file_path = f"{current_time.year}/{current_time.month}/{current_time.day}/{current_content_no}.parquet"
-            csv_file_path = f"{current_time.year}/{current_time.month}/{current_time.day}/{current_content_no}.csv"
-            parquet_folder = os.path.join(data_dir, os.path.dirname(parquet_file_path))
-            os.makedirs(parquet_folder, exist_ok=True)
-
-            df = pd.DataFrame([post])
-            df.to_parquet(f'{data_dir}/{parquet_file_path}', engine="pyarrow", compression="snappy")
-            df.to_csv(f'{data_dir}/{csv_file_path}', index=False)
-            saved_file_paths.append(parquet_file_path)
-
-            logger.info(f"Backed up post {current_content_no} to {parquet_file_path} and {csv_file_path}")
+            
+            backed_up_posts.append(post)
+            logger.info(f"Backed up post {current_content_no}")
 
         current_content_no -= 1
+        data_dir = Variable.get("data_dir")
+        df = pd.DataFrame(backed_up_posts)
+        
+        parquet_file_path = f"{start_content_no}_{current_content_no}.parquet"
+        csv_file_path = f"{start_content_no}_{current_content_no}.csv"
+
+        df.to_parquet(f'{data_dir}/{parquet_file_path}', engine="pyarrow", compression="snappy")
+        df.to_csv(f'{data_dir}/{csv_file_path}', index=False)
+        
         Variable.set("content_no", current_content_no)
         logger.info(f"Updated content_no to {current_content_no}")
 
-        return saved_file_paths
+        return [parquet_file_path]
 
     @task
     def upload_backup_to_gcs(files: list, bucket_name: str) -> None:
-        storage.upload_files_to_gcs(files, bucket_name)
+        storage.upload_files_to_gcs(files, bucket_name, "archive/post")
 
     @task
     def load_backup_to_bq(bucket_name: str, dataset_id: str, table_id: str, **kwargs) -> None:
-        operator = storage.load_files_to_bigquery(bucket_name, dataset_id, table_id)
+        gcs_source_uri = f"gs://{bucket_name}/archive/post/*.parquet"
+        operator = storage.load_files_to_bigquery(gcs_source_uri, dataset_id, table_id)
         operator.execute(context=kwargs)
 
     bucket_name = "blackdesert-mobile-hub-scraping-data-bucket"
     dataset_id = "blackdesert_mobile_hub_data"
-    table_id = "blackdesert_mobile_hub_scraping_backup"
+    table_id = "blackdesert_mobile_hub_scraping"
 
     files = backup_past_data()
     upload_backup_to_gcs(files, bucket_name) >> load_backup_to_bq(bucket_name, dataset_id, table_id)
