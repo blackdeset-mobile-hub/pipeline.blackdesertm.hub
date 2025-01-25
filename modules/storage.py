f8a9948043c29f3c98491d2c5de0edab35e5c103
Rooam Lee <leewr9@naver.com>
2025-01-25 13:31:32 +0900

    storage:: add dataframe

--- modules/storage.py
+++ modules/storage.py
@@ -1,11 +1,27 @@
 import logging
-import pendulum
+import pandas as pd
+
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.models import Variable
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
+
+def save_dataframe_to_files(data: dict, parquet_file_name: str, csv_file_name: str) -> None:
+    """
+    Save a dictionary as a DataFrame into Parquet and CSV files.
+    """
+    df = pd.DataFrame(data)
+    data_dir = Variable.get("data_dir")
+
+    if "date" in df:
+        df["date"] = pd.to_datetime(df["date"])
+    df['created_at'] = datetime.now()
+
+    df.to_parquet(f"{data_dir}/{parquet_file_name}", engine="pyarrow", compression="snappy")
+    df.to_csv(f"{data_dir}/{csv_file_name}", index=False)
 
 def upload_files_to_gcs(files_to_upload: list, bucket_name: str, bucket_path: str) -> None:
     """
@@ -30,7 +46,7 @@ def load_files_to_bigquery(gcs_source_uri: str, dataset_id: str, table_id: str) 
 
     load_operator = BigQueryInsertJobOperator(
         task_id="load_to_bigquery",
-        configuration={  # BigQuery load configuration
+        configuration={
             "load": {
                 "sourceUris": [gcs_source_uri],
                 "destinationTable": {
